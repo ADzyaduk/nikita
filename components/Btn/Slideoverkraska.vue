1917ecc8e87ceb147ef8f55dba54fa30c09b3d3b
AlexanderD <avdzyaduk@gmail.com>
2024-01-23 13:19:58 +0300

    231

---
+++ components/Btn/Slideoverkraska.vue
@@ -0,0 +1,16 @@
+<script setup>
+const isOpen = ref(false)
+</script>
+
+<template>
+    <div>
+        <UButton label="Узнать больше" variant="outline" icon="i-heroicons-arrow-right-20-solid" :trailing="true"
+            color="lime" @click="isOpen = true" />
+
+        <USlideover v-model="isOpen">
+            <div class="p-4 flex-1">
+                
+            </div>
+        </USlideover>
+    </div>
+</template>
